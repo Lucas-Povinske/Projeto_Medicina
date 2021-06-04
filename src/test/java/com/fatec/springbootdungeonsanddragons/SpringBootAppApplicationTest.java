997ed2c3f733de77a0adaef12de76780ab87b250
Lucas José Povinske <lucasjp96@gmail.com>
2021-06-04 18:38:21 -0300

    Adding to test

---
+++ src/test/java/com/fatec/springbootdungeonsanddragons/SpringBootAppApplicationTest.java
@@ -0,0 +1,33 @@
+//package com.fatec.springbootdungeonsanddragons.entity;
+
+//import java.util.List;
+//import java.util.HashSet;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.transaction.annotation.Transactional;
+//import static org.junit.jupiter.api.Assertions.*;
+
+//import com.fatec.springbootdungeonsanddragons.entity.Usuario;
+//import com.fatec.springbootdungeonsanddragons.entity.Personagem;
+//import com.fatec.springbootdungeonsanddragons.repository.UsuarioRepository;
+
+//@SpringBootTest
+//class SpringBootAppApplicationTests {
+
+ //   @Autowired
+//    private UsuarioRepository usuarioRepo;
+
+    // TESTES DE USUARIO
+//    @Test
+//	void contextLoads() {
+//    }
+//    @Test
+//    void testaInsercao() {
+//        Usuario usuario = new Usuario();
+//        usuario.setNomeUsuario("usuario_teste");
+//        usuario.setSenha("senha");
+//        usuario.setNomeExibicao("teste");
+//    }
+//}
